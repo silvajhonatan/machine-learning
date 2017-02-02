3d230b225b65bf09df34b0c7cc730abe318e563a
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-02-02 11:08:50 -0200

    update

--- 0-gradientDescent/gradientDescent.py
+++ 0-gradientDescent/gradientDescent.py
@@ -1,37 +1,48 @@
 """
 Jhonatan da Silva
 Last Updated version :
-Thu Feb  2 09:30:58 2017
+Thu Feb  2 11:08:49 2017
 Number of code lines: 
-30
+41
 """
+import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import bokeh.plotting as bp
 from matplotlib import style
 import numpy as np
 import random
 
-style.use('fivethirtyeight')
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-
+#style.use('fivethirtyeight')
 class gradientDescent():
 
     def __init__(self):
         self.x = np.linspace(-10,10,100)
-        self.y = self.x**3
+        self.y = self.x**2
         self.xdot = random.choice(self.x) 
         self.ydot = 0
-        p
+        self.mins = []
+        self.cost = []
 
-    def animate(self,i):
-        self.ydot = self.xdot**3
-        step = 0.1
-        ax1.clear()
-        plt.plot(self.x,self.y)
-        plt.plot(self.xdot,self.ydot,marker='o')
-        self.xdot += step
-
+    def derivative(self,x):
+        #test function = x^2
+        return 2*x
+    
+    def GD(self):
+        print('Initializing Gradient Descent')
+        oldMin = 0
+        currentMin = 7
+        #precision
+        epsilon = 0.001
+        step = 0.01
+        while abs(currentMin - oldMin) > epsilon:
+            oldMin = currentMin
+            gradient = self.derivative(oldMin)
+            move = gradient * step
+            currentMin = oldMin - move
+            self.cost.append((3-currentMin)**2)
+            self.mins.append(currentMin)
+        print('Local min : {:.2f}'.format(currentMin))
+    
 gradient = gradientDescent() 
-ani = animation.FuncAnimation(fig,gradient.animate)
-plt.show()
+gradient.GD()
